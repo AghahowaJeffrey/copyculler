d0946f17c4558765c85b414d7b9a2b47593eb935
Aghahowa Jeffrey <aghahowaj01@gmail.com>
2025-03-12 07:28:30 +0100

    removed previous backend code

--- copyculler/src/copyculler/main.py
+++ copyculler/src/copyculler/main.py
@@ -1,6 +1,6 @@
 from textual.app import App, ComposeResult
 from textual.containers import HorizontalGroup, VerticalGroup
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Header, Button, Input
 
 class Copyculler(App):
     """A RAG chatbot that lives in the terminal"""
@@ -9,7 +9,9 @@ class Copyculler(App):
     def compose(self) -> ComposeResult:
         """Create child widgets for the app"""
         yield Header()
+        yield InputSection()
         yield Footer()
+        
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
@@ -18,6 +20,16 @@ class Copyculler(App):
         )
 
 
+class InputSection(HorizontalGroup):
+    """The user input widget"""
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets input section"""
+        print('working')
+        yield HorizontalGroup(Input(placeholder="Enter your message.",
+                                    type="text",
+                                    name="message",
+                                    id="input-message"))
 
 if __name__ == "__main__":
     app = Copyculler()
